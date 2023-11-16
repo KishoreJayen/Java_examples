89d29e6f75335729c8e87d745041011b9edf0374
Kishore <Kishorejayen2020@gmail.com>
2023-11-16 14:03:57 +0530

    StackExample

--- src/Queue/QueueExample.java
+++ src/Queue/QueueExample.java
@@ -1,10 +1,38 @@
 package Queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Queue<Integer> que =  new LinkedList<Integer>();		
+		que.offer(20);
+		que.offer(40);
+		que.offer(50);
+		que.offer(60);
+		
+		System.out.println(que);
+		
+	que.poll()	;
+	System.out.println(que);
+	System.out.println("Peek: "
+			+que.peek());
 
+	System.out.println("The Size of the array is: "+ que.size());
+	
+	System.out.println(que.remove(2));
+	
+	System.out.println(que);
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
