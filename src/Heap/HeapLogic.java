42509d6faed8490d3533a5de10b8784f3825c330
Kishore <Kishorejayen2020@gmail.com>
2023-12-15 23:14:48 +0530

    dsdhhd

--- src/Heap/HeapLogic.java
+++ src/Heap/HeapLogic.java
@@ -1,5 +1,22 @@
 package Heap;
 
+import java.util.Scanner;
+
 public class HeapLogic {
 
-}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String fruits= sc.next();
+		
+		switch(fruits) {
+		
+		case "4" -> System.out.println("I am a Little Star");
+		case "Acha" -> System.out.println("Acha is Jayakumar");
+		
+			default ->System.out.println("Enter the order");
+		}
+		
+	}}
